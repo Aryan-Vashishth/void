3e2ca89342ada3494c92474885277627355f4647
Aryan-Vashishth <aryanvashishth360@gmail.com>
2026-05-01 13:14:48 +0530

    test: add VoidDemo smoke-test entry point Simple main() demonstrating VOID instantiation via DriverFactory.fromProfile(DEFAULT).

---
+++ src/test/java/core/misc/VoidDemo.java
@@ -0,0 +1,11 @@
+package core.misc;
+
+import WebApplication.VOID;
+import core.driver.DriverFactory;
+
+public class VoidDemo {
+    public static void main(String[] args) {
+        VOID app = new VOID();
+        System.out.println("VOID instance created: " + app);
+    }
+}
