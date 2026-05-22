113c5b5ed2c9425ce9e59263a296d878c9b51a76
Aryan-Vashishth <aryanvashishth360@gmail.com>
2026-05-22 14:13:50 +0530

    refactor: move cucumber StepDefinition package under core adapters

---
+++ src/main/java/core/adapters/cucumber/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Cucumber step-definition classes that wire BDD feature files to the
+ * {@link core.interactions.Interactions} and {@link dsl.VoidDSL}
+ * layers.
+ */
+package core.adapters.cucumber;
